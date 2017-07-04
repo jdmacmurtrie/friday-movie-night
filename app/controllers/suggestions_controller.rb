caa8ec99f60b6e5bf0b63cb156e21eed4f6d2d2e
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-07-04 17:09:35 -0400

    codeclimate

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -6,14 +6,16 @@ class SuggestionsController < ApplicationController
   def show
     combo = params[:id].split(',')
     Suggestion.create(
-    genre: Genre.find_by(name: combo.first),
-    topping: Topping.find_by(name: combo[1]),
-    user: current_user)
+      genre: Genre.find_by(name: combo.first),
+      topping: Topping.find_by(name: combo[1]),
+      user: current_user
+    )
     if combo.last != 'none'
       Suggestion.create(
-      genre: Genre.find_by(name: combo.first),
-      topping: Topping.find_by(name: combo.last),
-      user: current_user)
+        genre: Genre.find_by(name: combo.first),
+        topping: Topping.find_by(name: combo.last),
+        user: current_user
+      )
     end
     redirect_to root_path
   end
