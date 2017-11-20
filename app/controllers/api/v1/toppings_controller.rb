d8283ef3fd98808408bb701b251707abe3056600
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-11-20 14:36:36 -0500

    bug fix

--- app/controllers/api/v1/toppings_controller.rb
+++ app/controllers/api/v1/toppings_controller.rb
@@ -1,50 +1,50 @@
 class Api::V1::ToppingsController < ApplicationController
   def index
-		determine_toppings
+    determine_toppings
 
     if current_user
       pick_user_combo
     else
-			pick_genre_suggestion
+      pick_genre_suggestion
     end
 
     key = ENV["TMDB_KEY"]
     genres = HTTParty.get("https://api.themoviedb.org/3/genre/movie/list?api_key=#{key}")
-		get_genre_id(genres)
+    get_genre_id(genres)
 
     movies = HTTParty.get("https://api.themoviedb.org/3/genre/#{@genre_id}/movies?api_key=#{key}&page=#{random_number}")
-		four_movies = movies.parsed_response['results'].sample(4)
+    four_movies = movies.parsed_response['results'].sample(4)
 
-		@movie_suggestions = four_movies.map do |movie|
-			Movie.create(
-				title: movie['title'],
-				description: movie['overview'],
-				poster: movie['poster_path'],
-				genre: @genre_suggestion
-			)
-		end
+    @movie_suggestions = four_movies.map do |movie|
+      Movie.create(
+        title: movie['title'],
+        description: movie['overview'],
+        poster: movie['poster_path'],
+        genre: @genre_suggestion
+      )
+    end
     render json: { movies: @movie_suggestions }
   end
 
-		private
+  private
 
-	def get_genre_id(genres)
-		genres.parsed_response['genres'].each do |genre|
+  def get_genre_id(genres)
+    genres.parsed_response['genres'].each do |genre|
       if genre['name'] == @genre_suggestion.name
         @genre_id = genre['id']
       end
-		end
-	end
+    end
+  end
 
-	def determine_toppings
-		selected_toppings = params[:params].split(',')
-		@toppings = []
-		selected_toppings.each do |topping|
-			unless topping.blank?
-				@toppings << Topping.find_by(name: topping)
-			end
-		end
-	end
+  def determine_toppings
+    selected_toppings = params[:params].split(',')
+    @toppings = []
+    selected_toppings.each do |topping|
+      unless topping.blank?
+        @toppings << Topping.find_by(name: topping)
+      end
+    end
+  end
 
   def random_number
     rand(10)+1
@@ -57,20 +57,21 @@ class Api::V1::ToppingsController < ApplicationController
     @genre_suggestion = user_genres.sample
   end
 
-	def pick_genre_suggestion
-		all_suggestions = @toppings.map { |topping| topping.genres }
-		one_suggestion = all_suggestions.inject(:&)
-		if !one_suggestion.empty?
-			@genre_suggestion = one_suggestion[0]
-			how_many_suggestions
-		else
-			@genre_suggestion = all_suggestions.flatten.sample
-		end
-	end
+  def pick_genre_suggestion
+    all_suggestions = @toppings.map { |topping| topping.genres }
+    simplified_suggestion = all_suggestions.inject(:&)
+    binding.pry
+    if !simplified_suggestion.empty?
+      @genre_suggestion = simplified_suggestion[0]
+      how_many_suggestions(simplified_suggestion)
+    else
+      @genre_suggestion = all_suggestions.flatten.sample
+    end
+  end
 
-	def how_many_suggestions
-		if one_suggestion.length > 1
-			@genre_suggestion = one_suggestion.sample
-		end
-	end
+  def how_many_suggestions(simplified_suggestion)
+    if simplified_suggestion.length > 1
+      @genre_suggestion = simplified_suggestion.sample
+    end
+  end
 end
