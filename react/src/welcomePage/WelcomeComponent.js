c3f883eb57f7ae0aed4d99152bd6ea6c70336f89
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2018-03-07 23:01:22 -0500

    swap foundation with skeleton, further format welcome page

--- react/src/welcomePage/WelcomeComponent.js
+++ react/src/welcomePage/WelcomeComponent.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { browserHistory, Link } from 'react-router'
+// import pizza from 'images/pizza'
+// import film from 'images/film'
 
 const WelcomeComponent = (props) => {
   return(
     <div>
       <div className="choices row">
         <Link to='/toppings/new'>
-          <div className="need-movie small-6 columns">
-            Recommend a movie
+          <div className="need-movie">
+            Find me a movie
             <br/>
             based on my pizza
+            <br/>
+            <img src='/assets/film' alt="film reel" height="150" width="150"/>
           </div>
         </Link>
         <Link to='/movies/new'>
-          <div className="need-pizza small-6 columns">
-            Recommend a pizza
+          <div className="need-pizza">
+            Find me a pizza
             <br/>
             based on a movie
+            <br/>
+            <img src='/assets/pizza' alt="pizza" height="150" width="150"/>
           </div>
         </Link>
       </div>
@@ -26,7 +32,7 @@ const WelcomeComponent = (props) => {
 
 export default WelcomeComponent
 
-// 
+//
 // <div className="top-bar get-started">
 // <div>Friday Movie Night</div>
 // </div>
