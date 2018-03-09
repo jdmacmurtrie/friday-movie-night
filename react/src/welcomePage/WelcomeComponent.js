2a92bb2bae5380a373f67dfb0298c18c645a16aa
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2018-03-09 07:55:06 -0500

    roughly set up nav bar

--- react/src/welcomePage/WelcomeComponent.js
+++ react/src/welcomePage/WelcomeComponent.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { browserHistory, Link } from 'react-router'
-// import pizza from 'images/pizza'
-// import film from 'images/film'
 
 const WelcomeComponent = (props) => {
   return(
@@ -13,7 +11,7 @@ const WelcomeComponent = (props) => {
             <br/>
             based on my pizza
             <br/>
-            <img src='/assets/film' alt="film reel" height="150" width="150"/>
+            <img src='/assets/icons8-film-reel-filled-100' alt="film reel" height="150" width="150"/>
           </div>
         </Link>
         <Link to='/movies/new'>
@@ -22,7 +20,7 @@ const WelcomeComponent = (props) => {
             <br/>
             based on a movie
             <br/>
-            <img src='/assets/pizza' alt="pizza" height="150" width="150"/>
+            <img src='/assets/pizza-slice-combo-clipart' alt="pizza" height="150" width="150"/>
           </div>
         </Link>
       </div>
